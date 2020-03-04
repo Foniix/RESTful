9ccba5b75f1cadd671a775243e377db39c99b4c5
Fo_nix <d.gorbach.step@gmail.com>
2020-03-04 12:28:13 +0200

    Add files via upload

---
+++ PetsAPI/Data/SeedData.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using PetsAPI.Models;
+
+namespace PetsAPI.Data
+{
+    public static class SeedData
+    {
+        public static void Initialize(StenPetsContext context)
+        {
+            if (!context.Products.Any())
+            {
+                context.Products.AddRange(
+                    new Product
+                    {
+                        Name = "Squeaky Bone",
+                        Price = 20.99m
+                    },
+                    new Product
+                    {
+                        Name = "Knotted Rope",
+                        Price = 12.99m
+                    }
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ PetsAPI/Data/StenPetsContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetsAPI.Models;
+
+namespace PetsAPI.Data
+{
+    public class StenPetsContext : DbContext
+    {
+        public StenPetsContext(DbContextOptions<StenPetsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
